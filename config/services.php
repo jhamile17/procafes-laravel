d9d70d514cdb203fc2175333fdc1af5cf61fafef
Sayuri <i2413259@continental.edu.pe>
2026-06-29 22:52:55 -0500

    Agregue service.php

--- config/services.php
+++ config/services.php
@@ -66,4 +66,9 @@ return [
         'api_key' => env('GEMINI_API_KEY'),
         'model' => env('GEMINI_MODEL', 'gemini-2.5-flash'),
     ],
+
+        'groq' => [
+        'api_key' => env('GROQ_API_KEY'),
+        'model' => env('GROQ_MODEL', 'llama-3.3-70b-versatile'),
+    ],
 ];
