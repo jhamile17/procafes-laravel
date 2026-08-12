f353eb318cbc50967761a7793697c1052d2077ed
jhamile17 <https://github.com/jhamile17/proyectocoolify.git>
2026-08-12 12:24:32 -0500

    perfil -orders

--- resources/views/components/navbar/dropdown.blade.php
+++ resources/views/components/navbar/dropdown.blade.php
@@ -1,68 +1,60 @@
-{{--==========================================================================
-    NAVBAR DROPDOWN
-    --------------------------------------------------------------------------
-    Menú desplegable del usuario autenticado.
-==========================================================================--}}
-
 <ul class="dropdown-menu dropdown-menu-end navbar-dropdown">
 
     <li>
-
         <a
             href="{{ route('customer.profile') }}"
-            class="dropdown-item navbar-dropdown-item">
+            class="navbar-dropdown-item"
+        >
+            <span class="navbar-dropdown-icon">
+                <i class="bi bi-person" aria-hidden="true"></i>
+            </span>
 
-            <i
-                class="bi bi-person"
-                aria-hidden="true">
-            </i>
-
-            <span>Mi perfil</span>
-
+            <span class="navbar-dropdown-text">
+                Mi perfil
+            </span>
         </a>
-
     </li>
 
     <li>
-
         <a
             href="{{ route('customer.orders') }}"
-            class="dropdown-item navbar-dropdown-item">
+            class="navbar-dropdown-item"
+        >
+            <span class="navbar-dropdown-icon">
+                <i class="bi bi-bag" aria-hidden="true"></i>
+            </span>
 
-            <i
-                class="bi bi-bag"
-                aria-hidden="true">
-            </i>
-
-            <span>Mis pedidos</span>
-
+            <span class="navbar-dropdown-text">
+                Mis pedidos
+            </span>
         </a>
-
     </li>
-    <li class="navbar-dropdown-divider"></li>
-    <li>
 
+    <li class="navbar-dropdown-divider"></li>
+
+    <li>
         <form
             action="{{ route('logout') }}"
-            method="POST">
-
+            method="POST"
+        >
             @csrf
 
             <button
                 type="submit"
-                class="dropdown-item navbar-dropdown-item">
+                class="navbar-dropdown-item navbar-dropdown-logout"
+            >
+                <span class="navbar-dropdown-icon">
+                    <i
+                        class="bi bi-box-arrow-right"
+                        aria-hidden="true"
+                    ></i>
+                </span>
 
-                <i
-                    class="bi bi-box-arrow-right"
-                    aria-hidden="true">
-                </i>
-
-                <span>Cerrar sesión</span>
-
+                <span class="navbar-dropdown-text">
+                    Cerrar sesión
+                </span>
             </button>
-
         </form>
-
     </li>
 
 </ul>
