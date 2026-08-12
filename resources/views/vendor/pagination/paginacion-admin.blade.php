cbe60d57c058d4bffa403d96b605ede9eda42b02
jhamile17 <https://github.com/jhamile17/proyectocoolify.git>
2026-08-12 13:50:07 -0500

    paginacion-admin

--- resources/views/vendor/pagination/paginacion-admin.blade.php
+++ resources/views/vendor/pagination/paginacion-admin.blade.php
@@ -1,47 +1,150 @@
 @if ($paginator->hasPages())
 
-<nav class="paginacion-admin" role="navigation" aria-label="Paginación">
+    <nav
+        class="paginacion-admin"
+        role="navigation"
+        aria-label="Paginación"
+    >
 
-    <ul class="paginacion-admin-lista">
+        <ul class="paginacion-admin-lista">
 
-        {{-- Página anterior --}}
-        @if ($paginator->onFirstPage())
+            {{-- =====================================================
+                 PÁGINA ANTERIOR
+            ====================================================== --}}
 
-            <li class="paginacion-admin-item deshabilitado">
-
-                <span class="paginacion-admin-enlace">
-                    ← Anterior
-                </span>
-
-            </li>
-
-        @else
-
-            <li class="paginacion-admin-item">
-
-                <a
-                    href="{{ $paginator->previousPageUrl() }}"
-                    rel="prev"
-                    class="paginacion-admin-enlace">
-
-                    ← Anterior
-
-                </a>
-
-            </li>
-
-        @endif
-
-        {{-- Números --}}
-        @foreach ($elements as $element)
-
-            @if (is_string($element))
+            @if ($paginator->onFirstPage())
 
                 <li class="paginacion-admin-item deshabilitado">
 
                     <span class="paginacion-admin-enlace">
 
-                        {{ $element }}
+                        ← Anterior
+
+                    </span>
+
+                </li>
+
+            @else
+
+                <li class="paginacion-admin-item">
+
+                    <a
+                        href="{{ $paginator->previousPageUrl() }}"
+                        rel="prev"
+                        class="paginacion-admin-enlace"
+                        aria-label="Página anterior"
+                    >
+
+                        ← Anterior
+
+                    </a>
+
+                </li>
+
+            @endif
+
+
+            {{-- =====================================================
+                 NÚMEROS DE PÁGINA
+            ====================================================== --}}
+
+            @foreach ($elements as $element)
+
+                {{-- Separador ... --}}
+
+                @if (is_string($element))
+
+                    <li class="paginacion-admin-item deshabilitado">
+
+                        <span class="paginacion-admin-enlace">
+
+                            {{ $element }}
+
+                        </span>
+
+                    </li>
+
+                @endif
+
+
+                {{-- Páginas --}}
+
+                @if (is_array($element))
+
+                    @foreach ($element as $page => $url)
+
+                        @if ($page == $paginator->currentPage())
+
+                            {{-- Página actual --}}
+
+                            <li class="paginacion-admin-item activa">
+
+                                <span
+                                    class="paginacion-admin-enlace"
+                                    aria-current="page"
+                                >
+
+                                    {{ $page }}
+
+                                </span>
+
+                            </li>
+
+                        @else
+
+                            {{-- Página disponible --}}
+
+                            <li class="paginacion-admin-item">
+
+                                <a
+                                    href="{{ $url }}"
+                                    class="paginacion-admin-enlace"
+                                    aria-label="Ir a página {{ $page }}"
+                                >
+
+                                    {{ $page }}
+
+                                </a>
+
+                            </li>
+
+                        @endif
+
+                    @endforeach
+
+                @endif
+
+            @endforeach
+
+
+            {{-- =====================================================
+                 PÁGINA SIGUIENTE
+            ====================================================== --}}
+
+            @if ($paginator->hasMorePages())
+
+                <li class="paginacion-admin-item">
+
+                    <a
+                        href="{{ $paginator->nextPageUrl() }}"
+                        rel="next"
+                        class="paginacion-admin-enlace"
+                        aria-label="Página siguiente"
+                    >
+
+                        Siguiente →
+
+                    </a>
+
+                </li>
+
+            @else
+
+                <li class="paginacion-admin-item deshabilitado">
+
+                    <span class="paginacion-admin-enlace">
+
+                        Siguiente →
 
                     </span>
 
@@ -49,76 +152,8 @@
 
             @endif
 
-            @if (is_array($element))
+        </ul>
 
-                @foreach ($element as $page => $url)
-
-                    @if ($page == $paginator->currentPage())
-
-                        <li class="paginacion-admin-item activa">
-
-                            <span class="paginacion-admin-enlace">
-
-                                {{ $page }}
-
-                            </span>
-
-                        </li>
-
-                    @else
-
-                        <li class="paginacion-admin-item">
-
-                            <a
-                                href="{{ $url }}"
-                                class="paginacion-admin-enlace">
-
-                                {{ $page }}
-
-                            </a>
-
-                        </li>
-
-                    @endif
-
-                @endforeach
-
-            @endif
-
-        @endforeach
-
-        {{-- Página siguiente --}}
-        @if ($paginator->hasMorePages())
-
-            <li class="paginacion-admin-item">
-
-                <a
-                    href="{{ $paginator->nextPageUrl() }}"
-                    rel="next"
-                    class="paginacion-admin-enlace">
-
-                    Siguiente →
-
-                </a>
-
-            </li>
-
-        @else
-
-            <li class="paginacion-admin-item deshabilitado">
-
-                <span class="paginacion-admin-enlace">
-
-                    Siguiente →
-
-                </span>
-
-            </li>
-
-        @endif
-
-    </ul>
-
-</nav>
+    </nav>
 
 @endif
