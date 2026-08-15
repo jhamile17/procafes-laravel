0ccffa9d6183779d2428924a92fcaea5d2052503
jhamile17 <i2414018@continental.edu.pe>
2026-08-15 01:46:22 -0500

    Refactor head.blade.php for improved structure

--- resources/views/components/layout/head.blade.php
+++ resources/views/components/layout/head.blade.php
@@ -1,11 +1,34 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>@yield('title', 'PROCAFES')</title>
+<meta
+    name="viewport"
+    content="width=device-width, initial-scale=1"
+>
 
-<link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
-<link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+<meta
+    name="csrf-token"
+    content="{{ csrf_token() }}"
+>
+
+<title>
+    @yield('title', 'PROCAFES')
+</title>
+
+<link
+    rel="icon"
+    type="image/png"
+    href="{{ asset('images/logo.png') }}"
+>
+
+<link
+    rel="icon"
+    href="{{ asset('favicon.ico') }}"
+    sizes="any"
+>
+
+@vite([
+    'resources/css/app.css',
+    'resources/js/app.js'
+])
 
 @stack('styles')
