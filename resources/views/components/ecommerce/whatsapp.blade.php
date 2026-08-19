f0debc43c0323a9b2acc220f6a1ed3ad90ef908c
Sayuri <i2413259@continental.edu.pe>
2026-08-19 17:21:42 -0500

    numero correjido

--- resources/views/components/ecommerce/whatsapp.blade.php
+++ resources/views/components/ecommerce/whatsapp.blade.php
@@ -1,7 +1,7 @@
 <div class="floating-buttons">
 
     {{-- WhatsApp arriba --}}
-    <a href="https://wa.me/51955236237?text=Hola%20quiero%20hacer%20un%20pedido"
+    <a href="https://wa.me/51955236237?text=Hola%20quiero%20consultar%20sobre%20mi%20pedido"
        target="_blank"
        class="floating-whatsapp"
        aria-label="WhatsApp">
