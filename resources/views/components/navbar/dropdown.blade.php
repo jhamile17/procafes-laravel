62628fe81f8ccb706af431ac226c50dd96312fb6
jhamile17 <https://github.com/jhamile17/proyectocoolify.git>
2026-08-23 13:31:28 -0500

    corregido navbar para admin

--- resources/views/components/navbar/dropdown.blade.php
+++ resources/views/components/navbar/dropdown.blade.php
@@ -1,60 +1,176 @@
 <ul class="dropdown-menu dropdown-menu-end navbar-dropdown">
 
-    <li>
-        <a
-            href="{{ route('customer.profile') }}"
-            class="navbar-dropdown-item"
-        >
-            <span class="navbar-dropdown-icon">
-                <i class="bi bi-person" aria-hidden="true"></i>
-            </span>
+    @php
+        $user = auth()->user();
+    @endphp
 
-            <span class="navbar-dropdown-text">
-                Mi perfil
-            </span>
-        </a>
-    </li>
 
-    <li>
-        <a
-            href="{{ route('customer.orders') }}"
-            class="navbar-dropdown-item"
-        >
-            <span class="navbar-dropdown-icon">
-                <i class="bi bi-bag" aria-hidden="true"></i>
-            </span>
+    {{-- ==========================================================
+        ADMINISTRADOR
+    =========================================================== --}}
 
-            <span class="navbar-dropdown-text">
-                Mis pedidos
-            </span>
-        </a>
-    </li>
+    @if($user && $user->isAdmin())
+
+        <li>
+
+            <a
+                href="{{ route('admin.dashboard') }}"
+                class="navbar-dropdown-item"
+            >
+
+                <span class="navbar-dropdown-icon">
+
+                    <i
+                        class="bi bi-speedometer2"
+                        aria-hidden="true">
+                    </i>
+
+                </span>
+
+                <span class="navbar-dropdown-text">
+
+                    Panel administrativo
+
+                </span>
+
+            </a>
+
+        </li>
+
+
+        <li>
+
+            <a
+                href="{{ route('customer.profile') }}"
+                class="navbar-dropdown-item"
+            >
+
+                <span class="navbar-dropdown-icon">
+
+                    <i
+                        class="bi bi-person"
+                        aria-hidden="true">
+                    </i>
+
+                </span>
+
+                <span class="navbar-dropdown-text">
+
+                    Mi perfil
+
+                </span>
+
+            </a>
+
+        </li>
+
+
+    {{-- ==========================================================
+        CLIENTE
+    =========================================================== --}}
+
+    @else
+
+        <li>
+
+            <a
+                href="{{ route('customer.profile') }}"
+                class="navbar-dropdown-item"
+            >
+
+                <span class="navbar-dropdown-icon">
+
+                    <i
+                        class="bi bi-person"
+                        aria-hidden="true">
+                    </i>
+
+                </span>
+
+                <span class="navbar-dropdown-text">
+
+                    Mi perfil
+
+                </span>
+
+            </a>
+
+        </li>
+
+
+        <li>
+
+            <a
+                href="{{ route('customer.orders') }}"
+                class="navbar-dropdown-item"
+            >
+
+                <span class="navbar-dropdown-icon">
+
+                    <i
+                        class="bi bi-bag"
+                        aria-hidden="true">
+                    </i>
+
+                </span>
+
+                <span class="navbar-dropdown-text">
+
+                    Mis pedidos
+
+                </span>
+
+            </a>
+
+        </li>
+
+    @endif
+
+
+    {{-- ==========================================================
+        SEPARADOR
+    =========================================================== --}}
 
     <li class="navbar-dropdown-divider"></li>
 
+
+    {{-- ==========================================================
+        CERRAR SESIÓN
+    =========================================================== --}}
+
     <li>
+
         <form
             action="{{ route('logout') }}"
             method="POST"
         >
+
             @csrf
 
             <button
                 type="submit"
                 class="navbar-dropdown-item navbar-dropdown-logout"
             >
+
                 <span class="navbar-dropdown-icon">
+
                     <i
                         class="bi bi-box-arrow-right"
-                        aria-hidden="true"
-                    ></i>
+                        aria-hidden="true">
+                    </i>
+
                 </span>
 
                 <span class="navbar-dropdown-text">
+
                     Cerrar sesión
+
                 </span>
+
             </button>
+
         </form>
+
     </li>
 
 </ul>
